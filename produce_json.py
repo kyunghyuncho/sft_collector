f4b63cd21f5646a496fb87945c01a10e7d9b5238
Kyunghyun Cho <kyunghyun.cho@nyu.edu>
2024-05-05 20:32:49 -0400

    support "instruction" in produce_json.py.

--- produce_json.py
+++ produce_json.py
@@ -2,7 +2,7 @@ import sqlite3
 import json
 import argparse
 
-def select_data(dataset_name, table_name, sqlite_file, json_file):
+def select_data(dataset_name, table_name, sqlite_file, json_file, instruction="Answer the question."):
     # Connect to the SQLite database
     conn = sqlite3.connect(sqlite_file)
     cursor = conn.cursor()
@@ -15,7 +15,7 @@ def select_data(dataset_name, table_name, sqlite_file, json_file):
     conn.close()
 
     # Convert the data to a list of dictionaries
-    input_output_pairs = [{'instruction': 'Answer the question.',
+    input_output_pairs = [{'instruction': instruction,
                            'input': row[0], 'output': row[1]} for row in data]
 
     # Write the data to a JSON file
@@ -29,7 +29,8 @@ if __name__ == "__main__":
     parser.add_argument("--table_name", default="datasets", nargs='?', help="Name of the table")
     parser.add_argument("--json_file", default="output.json", nargs='?', help="Output JSON file name")
     parser.add_argument("--dataset_name", default="Antibody", nargs='?', help="Name of the dataset")
+    parser.add_argument("--instruction", default="Answer the question.", nargs='?', help="Any dataset specific instruction")
     args = parser.parse_args()
 
     # Call the function with provided arguments
-    select_data(args.dataset_name, args.table_name, args.sqlite_file, args.json_file)
+    select_data(args.dataset_name, args.table_name, args.sqlite_file, args.json_file, args.instruction)
